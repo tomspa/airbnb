0d17df86a1fb045874a0f8aaa32c9698fa0174cc
528049 <528049@student.glu.nl>
2018-11-30 10:24:58 +0100

    3e commit

--- App.js
+++ App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
+import { FontAwesome } from '@expo/vector-icons';
 
 class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hallooo</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text></Text>
+			</View>
+		);
+	}
 };
 
 export default createBottomTabNavigator({
-  Home: App
-})
+	Home: App
+},
+	{
+		navigationOptions: ({ navgation }) => ({
+			tabBarIcon: ({ focused, tintColor }) => {
+				const { routeName } = navigation.state;
+				let iconName;
+				switch (routeName) {
+					case 'Home':
+						iconName = 'search';
+				}
+				return <FontAwesome name={iconName} size={25} color={tintColor} />
+			}
+		}),
+		tabBarOptions: {
+			activeTintColor: '#fd5c63',
+			inactiveTintColor: 'gray',
+		}
+	}
+);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
