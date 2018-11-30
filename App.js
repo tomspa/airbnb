a12e7c83aec3f33837363b30744ca3dbe5ce3f76
528049 <528049@student.glu.nl>
2018-11-30 09:14:05 +0100

    2e commit

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hallooo</Text>
       </View>
     );
   }
-}
+};
+
+export default createBottomTabNavigator({
+  Home: App
+})
 
 const styles = StyleSheet.create({
   container: {
